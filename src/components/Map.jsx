602f625c3113ea5e38b761e3219cc9f1bb349afb
Stefan Kubala <stevokubala4445@gmail.com>
2023-11-21 10:47:20 +0100

    add Form

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,16 +1,20 @@
 /* eslint-disable no-unused-vars */
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import styles from "./Map.module.css";
 
 export default function Map() {
   const [searchParams, setSearchParams] = useSearchParams();
+  const navigate = useNavigate();
 
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
 
   return (
-    <div>
-      <span>{lat}</span>
-      <span>{lng}</span>
+    <div className={styles.mapContainer} onClick={() => navigate("form")}>
+      <h1>Map</h1>
+      <h2>
+        Position : {lat}, {lng}
+      </h2>
     </div>
   );
 }
