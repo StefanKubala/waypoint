6ab74529ea8401d58986c3ae9057166cc68f8fad
Stefan Kubala <stevokubala4445@gmail.com>
2023-11-23 14:09:45 +0100

    add login functionality and add ProtectedRoute

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,19 +1,50 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../contexts/FakeAuthContext";
 import Navbar from "../components/Navbar";
 import styles from "./Login.module.css";
 
 export default function Login() {
+  const [email, setEmail] = useState("jack@example.com");
+  const [password, setPassword] = useState("testing");
+
+  const { login, isAuthenticated } = useAuth();
+  const navigate = useNavigate();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (email && password) login(email, password);
+  }
+
+  useEffect(
+    function () {
+      if (isAuthenticated) navigate("/app", { replace: true });
+    },
+    [isAuthenticated, navigate]
+  );
+
   return (
     <main className={styles.login}>
       <Navbar />
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <div className={styles.row}>
           <label htmlFor="email">Email address</label>
-          <input type="email" id="email" />
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            type="email"
+            id="email"
+          />
         </div>
 
         <div className={styles.row}>
           <label htmlFor="password">Password</label>
-          <input type="password" id="password" />
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            id="password"
+          />
         </div>
 
         <div>
---
+++ src/pages/ProtectedRoute.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable react/prop-types */
+import { useEffect } from "react";
+import { useAuth } from "../contexts/FakeAuthContext";
+import { useNavigate } from "react-router-dom";
+
+export default function ProtectedRoute({ children }) {
+  const { isAuthenticated } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(
+    function () {
+      if (!isAuthenticated) navigate("/");
+    },
+    [isAuthenticated, navigate]
+  );
+  console.log(isAuthenticated);
+  return isAuthenticated ? children : null;
+}
