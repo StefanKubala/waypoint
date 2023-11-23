57896e0f3f92f1973eab6a16ad833d628fc4cdf0
Stefan Kubala <stevokubala4445@gmail.com>
2023-11-23 14:33:13 +0100

    add PageNotFound styling

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,3 +1,19 @@
+import { useNavigate } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import styles from "./PageNotFound.module.css";
+
 export default function PageNotFound() {
-  return <div>PageNotFound</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className={styles.pageNotFound}>
+      <Navbar />
+      <div>
+        <h1>The page you’re looking for can’t be found.</h1>
+        <button className={styles.notFoundBtn} onClick={() => navigate("/")}>
+          Back to homepage
+        </button>
+      </div>
+    </div>
+  );
 }
